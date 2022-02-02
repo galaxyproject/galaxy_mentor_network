01c70ac15790b9ed3f63268e04efaf507f10ca46
Chinonyemildred <chinonyemildred@gmail.com>
2022-02-02 19:51:15 +0000

    Intalled BootstrapVue plugin

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import BootstrapVue from 'bootstrap-vue'
 
-export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+export default function (Vue) {
+  Vue.use(BootstrapVue)
 }
+
+
