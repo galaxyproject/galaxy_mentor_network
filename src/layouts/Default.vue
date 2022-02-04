9cd02bc2d4f071b10d2caeeaf48bc7257fbd3ad6
Bérénice Batut <bebatut@gmail.com>
2022-02-04 15:30:31 +0100

    Fix bootstrap, sass imports and fix the navbar

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,20 +1,31 @@
 <template>
-  <b-navbar variant="dark" type="dark">
-    <b-navbar-brand href="#">
-      <img src="assets/images/logos/colour_text.png" class="d-inline-block align-top" alt="GMN Logo">
-      Galaxy Mentor Network
-    <b-nav-item>About</b-nav-item>
-
-    <b-nav-item-dropdown text="GMN Guides" right>
-        <b-dropdown-item href="#">Application Guidelines</b-dropdown-item>
-        <b-dropdown-item href="#">Mentroship Guidelines</b-dropdown-item>
-      </b-nav-item-dropdown>
-    <b-nav-item>FAQ</b-nav-item>
-    <b-nav-item disabled>Project & Participants</b-nav-item>
-    <b-nav-item>Blog</b-nav-item>
-  </b-nav>
-    <slot/>
-  </div>
+    <div class="layout">
+        <header class="masthead">
+            <b-navbar class="container justify-content-center" toggleable="lg">
+                <b-navbar-brand href="#">
+                    <img src="assets/images/logos/colour_text.png" class="d-inline-block align-top" alt="GMN Logo">
+                  {{ $static.metadata.siteName }}
+                </b-navbar-brand>
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav id="navbar-menu">
+                        <b-nav-item href="/about/">About</b-nav-item>
+                        <b-nav-item-dropdown text="Guides" right>
+                            <b-dropdown-item href="#">Application Guidelines</b-dropdown-item>
+                            <b-dropdown-item href="#">Mentroship Guidelines</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                        <b-nav-item href="/faq/">FAQ</b-nav-item>
+                        <b-nav-item disabled>Project & Participants</b-nav-item>
+                        <b-nav-item href="/blog/">Blog</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </header>
+        <main id="maincontainer" class="container">
+            <slot />
+        </main>
+        <footer class="static-footer"/>
+    </div>
 </template>
 
 <static-query>
@@ -25,30 +36,3 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,8 +19,3 @@ export default {
 }
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
