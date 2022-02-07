235f1776fa159eb57a6baa2a6f99ddc34c03f462
Chinonyemildred <chinonyemildred@gmail.com>
2022-02-07 14:44:16 +0100

    Moved mentorship.md to content

---
+++ src/pages/Mentorship_guidelines.vue
@@ -0,0 +1,27 @@
+<template>
+    <Layout>
+        <h1 class="page-title">{{ $page.main.title }}</h1>
+        <div class="markdown" v-html="$page.main.content" />
+    </Layout>
+</template>
+
+<page-query>
+query {
+    main: insert (path: "/content/mentorship_guidelines/") {
+        id
+        title
+        content
+        fileInfo {
+            path
+        }
+    }
+}
+</page-query>
+
+<script>
+export default {
+    metaInfo: {
+        title: "GMN - Mentorship_guidelines"
+    }
+}
+</script>
