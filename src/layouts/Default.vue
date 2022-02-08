ccaed79f7839a0bade4d253a36e6d9fac323bda9
Chinonyemildred <chinonyemildred@gmail.com>
2022-02-08 12:54:16 +0100

    Moved footer to default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -28,9 +28,46 @@
         <main id="maincontainer" class="container">
             <slot />
         </main>
-        <footer class="static-footer"/>
+        <footer class="static-footer">
+          <b-container>
+             <h3>Help and Support</h3>
+             <!--Grid row-->
+             <b-row>
+                  <!--Grid column-->
+                  <b-col lg="6" md="6" sm="12">
+                 <h6> Project Coordinators</h6>
+
+                     <li><a href="https://matrix.to/#/@beatrizserrano:matrix.org">Beatriz Serrano-Solano</a></li>
+                     <li><a href="https://matrix.to/#/@bebatut:matrix.org">Berenice Batut</a></li>
+                     <li><a href="https:matrix.to/#/@assuntad23:matrix.org">Assunta DeSanto</a></li>
+
+                 <h6> Contact Us</h6>
+                 <p>
+                     <i class="fas fa-envelop"></i><a href="mailto:galaxy.mentoring@gmail.com">galaxy.mentoring@gmail.com</a>
+                     <br>
+                     <i class="fab fa-github"></i><a href="https://github.com/bebatut/galaxy_mentor_network">Galaxy_Mentor_Network</a>
+                   </p>
+                  </b-col>
+
+                   <!--Grid column-->
+                  <b-col lg="6" md="6" sm="12">
+                       <p>
+                         The <a href="#">FAQ</a> section contains answers to most of the questions on your mind. Do you want to report a problem or suggest an improvement? We'll appreciate if you <a href="https://github.com/bebatut/galaxy_mentor_network/issues">open an issue</a> on our GitHub repository and we'll attend to it as quickly as possible.
+                       </p>
+                  </b-col>
+                  <!--Grid column-->
+              </b-row>
+              <!--Grid row-->
+          </b-container>
+        </footer>
     </div>
 </template>
+
+<script>
+export default {
+  name: 'layout',
+}
+</script>
 
 <static-query>
 query {
@@ -39,4 +76,11 @@ query {
   }
 }
 </static-query>
+
+<style lang=".scss" scoped>
+  footer {
+    background-color:"#ece9ef";
+    padding: "20px"
+  }
+</style>
 
