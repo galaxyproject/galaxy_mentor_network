52eb3bf2913233f4727b88b4cda384143b8596cc
Chinonyemildred <chinonyemildred@gmail.com>
2022-02-14 13:57:24 +0100

    made iterations to index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,99 +3,137 @@
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     
-    <header class="header">
-    </header>
-      <body>
-        <b-container class="index-container">
-        <b-row>
-          <b-col>
-        <h1>Mentorship...for you!</h1>
-
-        <p>An all in one program targeted to speed up your growth!</p>
-        <a href="https://forms.gle/482EoBYtJozWpYCf9" class="btn" role="button" target="_blank">Apply Now</a>
-            </b-col>
-            <b-col>
-              <img src="/images/illustration.png" alt="two people sitting in front of a computer">
-          </b-col>
-          </b-row>
-        </b-container>
     
     <b-container class="index-container">
-     <b-card-group deck>
-         <b-card class="index-card"
-            title="Community"
-           img-src="/images/gcc2019-grouppicture.jpg"
-           img-alt="Community members at Galaxy Community Conference, 2019"
-           img-top
-           border-radius="10px"
-           >
-            <b-card-text>
-               Birthed from the Galaxy Community, with it at the center.
-             </b-card-text>
-          </b-card>
-          <b-card class="index-card"
-              title="Collaboration"
-             img-src="/images/linkedin-sales-solutions-QgYvORVDdd8-unsplash.jpg"
-             img-alt="Four hands joined together"
-             img-top
-             border-radius="10px"       
-              >
-             <b-card-text>
-                  The program is founded and sustained by the pillars of collaboration.
-              </b-card-text>
-           </b-card>
-           <b-card class="index-card"
-                title="Growth"
-                img-src="/images/krakenimages-Y5bvRlcCx8k-unsplash.jpg"
-                img-alt="Corporate lady smiling"
-                img-top
-                border-radius="10px"       
-                >
-                <b-card-text>
-                    Your growth and expectations are our focus, and your growth drives us.
-                 </b-card-text>
-           </b-card>
-     </b-card-group>
-    </b-container>
-     <b-container class="index-container">
-       <b-row class="index2-row">
+        <b-row class="index-row">
          <b-col>
-           <img src="/images/gcc2019-grouppicture.jpg" alt="image">
+           <h1>Mentorship...for you!</h1>
+
+           <p>An all in one program targeted to speed up your growth!</p>
+           <a href="https://forms.gle/482EoBYtJozWpYCf9" class="btn" role="button" target="_blank">Apply Now</a>
+         </b-col>
+         <b-col>
+              <g-image src="~/images/illustration.png" alt="two people sitting in front of a computer" width="400"/>
+           </b-col>
+        </b-row>
+  
+    <b-row class="index-row">
+     <b-col class="index-column">
+         <g-image src="~/images/undraw_connected_world.svg" alt="image showing connected world" width="200" class="index-image"/>
+         <p><strong>Community</strong></p>
+            <p>
+               Birthed from the Galaxy Community, with it at the center.
+             </p>
+     </b-col>
+     <b-col class="index-column">
+         <g-image src="~/images/undraw_collaborators.svg" alt="people working together" width="200" class="index-image"/>
+         <p><strong>Collaboration</strong></p>
+            <p>
+               The program is founded and sustained by the pillars of collaboration.
+             </p>
+     </b-col>
+     <b-col class="index-column">
+         <g-image src="~/images/undraw_progress_data.svg" alt="progress data chart" width="200" class="index-image"/>
+         <p><strong>Growth</strong></p>
+            <p>
+               Your growth and expectations are our focus, and your growth drives us.
+             </p>
+     </b-col>
+   </b-row>
+   <b-row class="index-row">
+         <b-col>
+           <g-image src="~/images/GMN text poster.jpg" alt="image showing words related to mentorship" width="400"/>
          </b-col>
          <b-col>
            <h3>About Galaxy Mentor Network Program</h3>
            <p>Galaxy Mentor Network (GMN) program is an initiative by the Galaxy Community to build her members in open source projects.</p> 
             <p>The GMN program is for everyone who needs guidance in an open source project, anyone who needs help learning how to contribute to open source projects, guidance in navigating and settling into the Galaxy ecosystem.</p>
-            <span class="fas fa-arrow-circle-right"></span>  <a href="/about/">Learn More</a>          
+            <span class="fas fa-arrow-circle-right"></span>   <a href="/about/">Learn More</a>          
          </b-col>
-       </b-row>
-     </b-container>
-     
-     <b-container class="index-container">
-       <b-row class="p-4">
+    </b-row>
+    <b-row class="index-row">
          <b-col>
            <h3>Program Highlights</h3>
            <p>This 8-week mentorship has been designed to ensure that our GMentors and GMentees have the flexibility to learn and grow together while they work towards their intended goals and expectations.</p>
          </b-col>
          <b-col>
-          <b-card class="index-card">
-            <b-row class="index-row">
-              <b-card-text class="index-card-text">Introductory Call- setting goals and schedules</b-card-text>
-            </b-row><br>
-            <b-row class="index-row">
-              <b-card-text class="index-card-text">Two other meetings with the Network Manager</b-card-text>
-            </b-row><br>
-            <b-row class="index-row">
-              <b-card-text class="index-card-text">Mid-program feedback</b-card-text>
-            </b-row><br>
-            <b-row class="index-row">
-              <b-card-text class="index-card-text">End of program feedback</b-card-text>
-            </b-row>
-           </b-card>    
+            <g-image src="~/images/Program timeline.png" alt="Image showing program highlights" width="400"/>
          </b-col>
        </b-row>
+    
+  <!--   Still finding this difficult, yet to figure it out correctly
+    <div class="index-row">
+            <div class="col-md-12">
+               <id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+               <ol class="carousel-indicators">
+                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                 <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                 <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  
+               </ol>
+               <div class="carousel-inner">
+                 <div class="carousel-item active">
+                   <div class="index-row">
+                     <div class="col-md-4">
+                       <div-class="image-area"><g-image src="~/images/illustration.png" alt="two people sitting in front of a computer"/>
+                          <div class="image-text">
+                            <p><strong>Jade Kim</strong></p>
+                            <p>GMentor</p>
+                            <p>Birthed from the Galaxy Community, with it at the center.</p>
+                          </div>
+                        </div>
+                     </div>
+                     <div class="col-md-4">
+                       <div-class="image-area"><g-image src="~/images/illustration.png" alt="two people sitting in front of a computer"/>
+                          <div class="image-text">
+                            <p><strong>Jade Kim</strong></p>
+                            <p>GMentor</p>
+                            <p>Birthed from the Galaxy Community, with it at the center.</p>
+                          </div>
+                        </div>
+                     </div>
+                     <div class="col-md-4">
+                       <div-class="image-area"><g-image src="~/images/illustration.png" alt="two people sitting in front of a computer"/>
+                          <div class="image-text">
+                            <p><strong>Jade Kim</strong></p>
+                            <p>GMentor</p>
+                            <p>Birthed from the Galaxy Community, with it at the center.</p>
+                          </div>
+                        </div>
+                     </div>
+                 <div class="carousel-item">
+                   <div class="index-row">
+                     <div class="col-md-4">
+                       <div-class="image-area"><g-image src="~/images/illustration.png" alt="two people sitting in front of a computer"/>
+                          <div class="image-text">
+                            <p><strong>Jade Kim</strong></p>
+                            <p>GMentor</p>
+                            <p>Birthed from the Galaxy Community, with it at the center.</p>
+                          </div>
+                        </div>
+                     </div>
+                     <div class="col-md-4">
+                       <div-class="image-area"><g-image src="~/images/illustration.png" alt="two people sitting in front of a computer"/>
+                          <div class="image-text">
+                            <p><strong>Jade Kim</strong></p>
+                            <p>GMentor</p>
+                            <p>Birthed from the Galaxy Community, with it at the center.</p>
+                          </div>
+                        </div>
+                     </div>
+                     <div class="col-md-4">
+                       <div-class="image-area"><g-image src="~/images/illustration.png" alt="two people sitting in front of a computer"/>
+                          <div class="image-text">
+                            <p><strong>Jade Kim</strong></p>
+                            <p>GMentor</p>
+                            <p>Birthed from the Galaxy Community, with it at the center.</p>
+                          </div>
+                        </div>
+                        </div>
+        </div>-->
      </b-container>
-   </body>
+   
+   
      
     
 
@@ -111,11 +149,4 @@ export default {
 }
 
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
-
 
