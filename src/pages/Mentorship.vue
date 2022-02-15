9c28c9be517ccaab9d068c8e4990af76db3b6b2e
Chinonyemildred <82679007+Chinonyemildred@users.noreply.github.com>
2022-02-15 10:28:31 +0100

    removed unnecessary spacing
    Co-authored-by: Bérénice Batut <berenice.batut@gmail.com>

--- src/pages/Mentorship.vue
+++ src/pages/Mentorship.vue
@@ -1,9 +1,7 @@
 <template>
      <Layout>
-   
             <h1 class="page-title">{{ $page.main.title }}</h1>
             <div class="markdown" v-html="$page.main.content" /> 
-        
      </Layout>
 
 </template>
